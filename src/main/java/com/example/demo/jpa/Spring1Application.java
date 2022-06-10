1a4a0e5721b7690f910a962e8a4253f31d200c39
K3EEE <k_emam@hotmail.com>
2022-06-10 21:47:33 +0300

    The first release

---
+++ src/main/java/com/example/demo/jpa/Spring1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Spring1Application.class, args);
+		
+		System.out.println("spring 1 execution");
+	}
+
+}
