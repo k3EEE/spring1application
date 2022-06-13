2fd0f6e969f0475f826bd9f69a45071b6f465452
k3EEE <102011478+k3EEE@users.noreply.github.com>
2022-06-14 00:47:41 +0300

    Update Testclass.java
    remotely changed

--- src/main/java/com/example/demo/jpa/Testclass.java
+++ src/main/java/com/example/demo/jpa/Testclass.java
@@ -4,5 +4,7 @@ public class Testclass {
 
 	Testclass() {
 		System.out.println("spring 1 execution");
+		System.out.println("remote github line 1");
+		System.out.println("remote github line 2");
 	}
 }
